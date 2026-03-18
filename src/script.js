feaeaac05592649dbe8c1872629ca6d2ccb4572f
Agyepong Jessica <jessicaagyepong53@gmail.com>
2026-03-18 14:43:06 +0000

    Done

--- src/script.js
+++ src/script.js
@@ -2,9 +2,7 @@ let username = prompt("What is your name?");
 alert (`Hi ${username}. Exicted to see you here💞...`);
 
 let age = 15;
-alert(`Your age is ${age}.`);
+alert(`You are ${age} years old.`);
 
 let email = prompt("we will keep in touch via your");
 alert(`we will keep in touch via your  ${email}.`);
-alert(`You are ${age} years old.`);
-
