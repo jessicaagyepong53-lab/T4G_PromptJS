b43165539d051c63718a2e8f23589f1b7fbb9611
Liya-Abu <abubakarmuslia1@gmail.com>
2026-03-18 14:51:34 -0700

    making changes

--- src/script.js
+++ src/script.js
@@ -3,3 +3,6 @@ alert (`Welcome to T4G ${username}. Exicted to see you here💞...`);
 
 let age = 15;
 alert(`Your age is ${age}.`);
+
+let email = prompt("we will keep in touch via your");
+alert(`we will keep in touch via your  ${email}.`);
